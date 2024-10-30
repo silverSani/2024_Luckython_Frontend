2b4b53b53f6a8478175a042be6c9cb2f1b9a355d
jieun <jer011025@naver.com>
2024-10-31 02:25:56 +0900

    feat : 지은 UI 완성

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import "styles/home.css";
 
-export default function Home(){
+export default function Home() {
+    const [fadeOut, setFadeOut] = useState(false);
 
+    useEffect(() => {
+        setTimeout(() => {
+            setFadeOut(true);
+        }, 850);
+    }, []);
 
-    return (
-        <div class="container">
-        <h1 class="logo">SoundP<span class="icon">i</span>n</h1>
-        <div class="search-bar">
-          <input type="text" placeholder="Search..." />
-        </div>
-        <Link to="/login">
-            guest 말고 owner로 시작하기
-        </Link>
-      </div>)
+    return (<div><div className={`fade-overlay ${fadeOut ? "fade-out" : ""}`} />
+
+        <div className="containerhome">
+            <img src="/images/soundpin.png" alt="SoundPin Logo" className="logo" />
+            <div className="search-bar">
+                <input type="text" placeholder="Search..." />
+            </div>
+            <Link to="/login" className="login-link">
+                guest 말고 owner로 시작하기
+            </Link>
+        </div></div>
+    );
 }
