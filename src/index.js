ca11ca06679ffdac2d5de6b906683411890dfde2
jieun <jer011025@naver.com>
2024-10-30 23:27:11 +0900

    jieun main init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import Router from "pages/app";
+import Router from "app";
 import "styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
